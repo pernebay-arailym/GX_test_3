f3b7c64c44f87b3fab09adc35ca798ff314667f3
pernebay-arailym <pernebayarailym@gmail.com>
2025-11-27 15:30:17 +0100

    refactor: clean up datasource definition and improve formatting

--- gx_main.py
+++ gx_main.py
@@ -1,18 +1,29 @@
 import great_expectations as gx
+print(gx.__version__)
 import pandas as pd 
 from pathlib import Path
 import os
+
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/arailym.pernebay/Documents/GX_test_projects/GX_test_3/credentials/gcs_key.json"
+
 #gx context setup
 context = gx.get_context(mode='file')
 
 #define a datasource
+#data_source_name = "flights_data_source"
+#bucket_or_name = "flights-dataset-yt-tutorial"
+#gcs_options = {}
+#data_source = context.data_sources.add_pandas_gcs(
+#    name="flights_data_source", bucket_or_name="flights-dataset-yt-tutorial", gcs_options = gcs_options
+#    )
+
 data_source_name = "flights_data_source"
 bucket_or_name = "flights-dataset-yt-tutorial"
 gcs_options = {}
 data_source = context.data_sources.add_pandas_gcs(
-    name = "flights_data_source", bucket_or_name = "flights-dataset-yt-tutorial", gcs_options = {}
-    )
+    name="flights_data_source", bucket_or_name="flights-dataset-yt-tutorial", gcs_options=gcs_options
+)
 
 #define a Data Asset
 asset_name = "goibibo_flights_data"
